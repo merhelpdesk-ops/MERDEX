508dd9f9aa41c87698430012afdf55b7aaed9d15
merhelpdesk <merhelpdesk@gmail.com>
2026-09-25 21:35:35 +0800

    Update Index.tsx

--- app/pages/swap/Index.tsx
+++ app/pages/swap/Index.tsx
@@ -44,22 +44,6 @@ export default function SwapIndex() {
             <WooFiWidget />
           </Suspense>
         </div>
-
-        {/* 底部 Bybit 广告横幅 */}
-        <div className="mt-12 flex w-full max-w-[1000px] justify-center px-2">
-          <a
-            href="https://partner.bybit.com/b/165473"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="w-full transition-transform hover:scale-[1.01]"
-          >
-            <img
-              src="/bybit-banner.JPG"
-              alt="Bybit Promotion"
-              className="w-full rounded-xl border border-white/10 object-cover shadow-lg"
-            />
-          </a>
-        </div>
       </div>
     </>
   );
